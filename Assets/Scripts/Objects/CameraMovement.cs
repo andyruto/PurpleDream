df76b509dc3d4871cd862dc16cdc57078d3fbe49
andyruto <andreasgantner@yahoo.de>
2018-06-24 12:24:30 +0200

    VS update and fixing building error
    During the Visual Studio for mac update there came up several changes in the projects code. Moreover I made a temporary fix for the camera movement script.

--- Assets/Scripts/Objects/CameraMovement.cs
+++ Assets/Scripts/Objects/CameraMovement.cs
@@ -25,14 +25,18 @@ class CameraMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Set camera position to player position (only y axe)
-		Vector3 playerInfo = player.transform.transform.position;
-		mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, playerInfo.y, playerInfo.z - cameraDistOffset);
+		//Vector3 playerInfo = player.transform.transform.position;
+		//mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, playerInfo.y, playerInfo.z - cameraDistOffset);
 	
+		//Set camera position temporary to player position
+		Vector3 playerInfo = player.transform.transform.position;
+        mainCamera.transform.position = new Vector3(playerInfo.x, playerInfo.y, playerInfo.z - cameraDistOffset);
+
 		//ToDo: Smooth CameraMovement if player moves to moving area of camera
 		//      Don't let camera move out of level range
-		if(playerInfo.x > (mainCamera.transform.position.x + (0.5f * (mainCamera. / 3f))))
-		{
-			mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + movingSpeed, playerInfo.y, playerInfo.z - cameraDistOffset);
-		}
+        //if(playerInfo.x > (mainCamera.transform.position.x + (0.5f * (Screen. / 3f))))
+		//{
+		//	mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + movingSpeed, playerInfo.y, playerInfo.z - cameraDistOffset);
+		//}
 	}
 }
